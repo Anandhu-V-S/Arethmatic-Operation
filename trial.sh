b3477026d4020900ca1e8fd5a71138757746f9a3
Anandhu <102431460+Anandhu-V-S@users.noreply.github.com>
2022-04-30 19:42:35 +0530

    added script

---
+++ trial.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+LIGHTGREEN="\e[92m"
+LIGHTRED="\e[91m"
+LIGHTCYAN="\e[96m"
+ENDCOLOR="\e[0m"
+printf "\e[1;92m[\e[0m\e[1;77m1\e[0m\e[1;92m]\e[0m\e[1;93m Addition\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m2\e[0m\e[1;92m]\e[0m\e[1;93m Substraction\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m3\e[0m\e[1;92m]\e[0m\e[1;93m Multiplication\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m4\e[0m\e[1;92m]\e[0m\e[1;93m Division\e[0m\n"
+read -p $'\n\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Choose an option: \e[0m\en' option
+if [[ $option == 1 ]]; then
+	echo -e "${LIGHTGREEN} Value of A"
+	read A 
+	echo -e "${LIGHTGREEN} Value of B"
+	read B
+	echo -e "${LIGHTGREEN}SUM=$(($A+$B))"
+fi
+
+if [[ $option == 2 ]]; then
+	echo -e "${LIGHTRED}Value of A"
+	read A
+	echo -e "${LIGHTRED}Value of B"
+	read B
+	echo -e "${LIGHTRED}Value=$(($A-$B))"
+fi
+if [[ $option == 3 ]]; then
+	echo -e "${LIGHTCYAN}Value of A"
+	read A
+	echo -e "${LIGHTCYAN}Value of B"
+	read B
+	echo -e "${LIGHTCYAN}Value=$(($A*$B))"
+fi
+if [[ $option == 4 ]]; then
+	echo "Value of A"
+	read A
+	echo "Value of B"
+	read B
+	echo "Value=$(($A%$B))"
+fi
+
+
+
